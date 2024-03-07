0ad0f68dfd8ad96d164f73a5476082aac9098b71
Rony Rizk <rony_rizk@live.com>
2024-03-07 15:24:48 +0200

    Loading on Assign Button

--- src/components/ir-channel/types.ts
+++
@@ -1,7 +0,0 @@
-export interface IModalCause {
-  cause: string;
-  action: () => Promise<any>;
-  title: string;
-  message: string;
-  main_color: 'primary' | 'danger';
-}
--- src/models/events.dto.ts
+++
@@ -1,7 +0,0 @@
-export interface ReallocateEventUpdateDTO {
-  oldPool: string;
-  newPool: string;
-  from_date: string;
-  to_date: string;
-  pr_id: number;
-}
