48a103819c61d8e41ac5b84ac4cc6c279dfd2bc8
Rony Rizk <rony_rizk@live.com>
2025-02-26 16:24:23 +0200

    uil fix

--- src/components/ui/ir-text-editor/ir-text-editor.tsx
+++ src/components/ui/ir-text-editor/ir-text-editor.tsx
@@ -1,4 +1,4 @@
-import { Component, h, Element, Event, EventEmitter, Prop, Watch } from '@stencil/core';
+import { Component, h, Element, Event, EventEmitter, Prop, Watch, State } from '@stencil/core';
 import Quill, { QuillOptions } from 'quill';
 
 export type QuillToolbarButton = 'bold' | 'italic' | 'underline' | 'strike' | 'link' | 'image' | 'video' | 'clean';
@@ -72,6 +72,8 @@ export class IrTextEditor {
   /** Emits current HTML content whenever it changes */
   @Event() textChange: EventEmitter<string>;
 
+  @State() editorValue = '';
+
   /** Private, non-reactive Quill editor instance */
   private editor: Quill;
 
@@ -80,13 +82,45 @@ export class IrTextEditor {
   componentDidLoad() {
     const options: QuillOptions = {
       modules: {
-        toolbar: this.computedToolbar,
+        toolbar: {
+          container: this.computedToolbar,
+          handlers: {
+            undo: () => {
+              if (this.editor) {
+                this.editor.history.undo();
+                this.updateHistoryButtons();
+              }
+            },
+            redo: () => {
+              if (this.editor) {
+                this.editor.history.redo();
+                this.updateHistoryButtons();
+              }
+            },
+          },
+        },
+        history: {
+          delay: 1000,
+          maxStack: 100,
+          userOnly: true,
+        },
       },
       placeholder: this.placeholder,
       readOnly: !this.userCanEdit || this.readOnly,
       theme: 'snow',
     };
+    const icons = Quill.import('ui/icons');
+    icons['undo'] =
+      '<svg title="undo" xmlns="http://www.w3.org/2000/svg" height="18" width="18" viewBox="0 0 512 512"><path class="ql-fill" d="M48.5 224L40 224c-13.3 0-24-10.7-24-24L16 72c0-9.7 5.8-18.5 14.8-22.2s19.3-1.7 26.2 5.2L98.6 96.6c87.6-86.5 228.7-86.2 315.8 1c87.5 87.5 87.5 229.3 0 316.8s-229.3 87.5-316.8 0c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0c62.5 62.5 163.8 62.5 226.3 0s62.5-163.8 0-226.3c-62.2-62.2-162.7-62.5-225.3-1L185 183c6.9 6.9 8.9 17.2 5.2 26.2s-12.5 14.8-22.2 14.8L48.5 224z" /></svg>'; // Replace '...' with actual SVG path
+    icons['redo'] =
+      '<svg title="redo" xmlns="http://www.w3.org/2000/svg" height="18" width="18" viewBox="0 0 512 512"><path class="ql-fill" d="M463.5 224l8.5 0c13.3 0 24-10.7 24-24l0-128c0-9.7-5.8-18.5-14.8-22.2s-19.3-1.7-26.2 5.2L413.4 96.6c-87.6-86.5-228.7-86.2-315.8 1c-87.5 87.5-87.5 229.3 0 316.8s229.3 87.5 316.8 0c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0c-62.5 62.5-163.8 62.5-226.3 0s-62.5-163.8 0-226.3c62.2-62.2 162.7-62.5 225.3-1L327 183c-6.9 6.9-8.9 17.2-5.2 26.2s12.5 14.8 22.2 14.8l119.5 0z"/></svg>';
     this.editor = new Quill(this.editorContainer, options);
+    (this.editor.getModule('toolbar') as any).addHandler('undo', () => {
+      this.editor.history.undo();
+    });
+    (this.editor.getModule('toolbar') as any).addHandler('redo', () => {
+      this.editor.history.redo();
+    });
     if (this.value) {
       this.setEditorValue(this.value);
     }
@@ -101,13 +135,18 @@ export class IrTextEditor {
         }
       }
       const html = this.editor.root.innerHTML;
+      this.editorValue = html;
       this.textChange.emit(html);
     });
   }
 
   @Watch('value')
   handleValueChange(newValue: string, oldValue: string) {
-    if (newValue !== oldValue) {
+    if (!newValue) {
+      this.setEditorValue(newValue);
+      return;
+    }
+    if (newValue !== oldValue && newValue !== this.editorValue) {
       this.setEditorValue(newValue);
     }
   }
@@ -131,7 +170,7 @@ export class IrTextEditor {
     }
   }
   private get computedToolbar() {
-    return this.toolbarConfig ? buildToolbar(this.toolbarConfig) : [['bold', 'italic', 'underline', 'strike'], ['link'], ['clean']];
+    return this.toolbarConfig ? buildToolbar(this.toolbarConfig) : [[{ undo: 'ql-undo' }, { redo: 'ql-redo' }], ['bold', 'italic', 'underline', 'strike'], ['link'], ['clean']];
   }
   private setEditorValue(value: string) {
     if (!this.editor) {
@@ -142,6 +181,25 @@ export class IrTextEditor {
       const length = this.editor.getLength();
       this.editor.setSelection(length - 1, 0);
     });
+  }
+  private updateHistoryButtons() {
+    const undoButton = this.el.querySelector('.ql-undo') as HTMLButtonElement;
+    const redoButton = this.el.querySelector('.ql-redo') as HTMLButtonElement;
+    if (!this.editor) return;
+
+    // Disable undo if there is no undo history
+    if (this.editor.history.stack.undo.length === 0) {
+      undoButton && (undoButton.disabled = true);
+    } else {
+      undoButton && (undoButton.disabled = false);
+    }
+
+    // Disable redo if there is no redo history
+    if (this.editor.history.stack.redo.length === 0) {
+      redoButton && (redoButton.disabled = true);
+    } else {
+      redoButton && (redoButton.disabled = false);
+    }
   }
   render() {
     return (
