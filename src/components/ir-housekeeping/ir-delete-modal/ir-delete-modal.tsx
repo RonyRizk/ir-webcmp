c575ac284df3d26d3cb5e5530d72121d5108b9a6
Rony Rizk <rony_rizk@live.com>
2025-08-01 14:32:10 +0300

    Fix Modal while removing house keeper

--- src/components/ir-housekeeping/ir-delete-modal/ir-delete-modal.tsx
+++ src/components/ir-housekeeping/ir-delete-modal/ir-delete-modal.tsx
@@ -20,27 +20,15 @@ export class IrDeleteModal {
   @Event() resetData: EventEmitter<string>;
 
   private housekeepingService = new HouseKeepingService();
-  private modalEl: HTMLDivElement;
 
   @Method()
   async closeModal() {
-    if (this.modalEl) {
-      $(this.modalEl).modal('hide');
-      this.isOpen = false;
-      this.modalClosed.emit(null);
-    }
+    this.isOpen = false;
+    this.modalClosed.emit(null);
   }
   @Method()
   async openModal() {
-    if (this.modalEl) {
-      $(this.modalEl).modal({
-        focus: true,
-        backdrop: true,
-        keyboard: true,
-      });
-      $(this.modalEl).modal('show');
-      this.isOpen = true;
-    }
+    this.isOpen = true;
   }
 
   @Listen('clickHandler')
@@ -76,41 +64,79 @@ export class IrDeleteModal {
     if (!this.user) {
       return;
     }
-    return (
-      <div aria-modal={this.isOpen ? 'true' : 'false'} class="modal fade" ref={el => (this.modalEl = el)} tabindex="-1">
-        <div class="modal-dialog modal-dialog-centered">
-          <div class="modal-content">
-            <div class="modal-body">
-              <div class={`ir-alert-header mb-1 d-flex align-items-center justify-content-between border-0 py-0 m-0 `}>
-                <p class="p-0 my-0 modal-title">{this.user.assigned_units.length > 0 ? locales.entries.Lcz_AssignUnitsTo : locales.entries.Lcz_ConfirmDeletion}</p>
-                <ir-button class="exit-icon" variant="icon" icon_name="xmark" onClickHandler={() => this.closeModal()}></ir-button>
+    return [
+      <div
+        class={`backdropModal ${this.isOpen ? 'active' : ''}`}
+        onClick={() => {
+          this.closeModal();
+        }}
+      ></div>,
+      <div data-state={this.isOpen ? 'opened' : 'closed'} class={`ir-modal`} tabindex="-1">
+        <div class={`ir-alert-content p-2`}>
+          <div class={`r-alert-header mb-1 d-flex align-items-center justify-content-between border-0 py-0 m-0 `}>
+            <p class={'m-0 p-0'}>{this.user.assigned_units.length > 0 ? locales.entries.Lcz_AssignUnitsTo : locales.entries.Lcz_ConfirmDeletion}</p>
+            <ir-button class="exit-icon" variant="icon" icon_name="xmark" onClickHandler={() => this.closeModal()}></ir-button>
+          </div>
+          <div class="modal-body text-left p-0 mb-2">
+            {this.user.assigned_units.length > 0 && (
+              <div class="modal-body text-left p-0 mb-2">
+                <ir-select
+                  firstOption={locales.entries.Lcz_nobody}
+                  selectedValue={this.selectedId}
+                  onSelectChange={e => (this.selectedId = e.detail)}
+                  LabelAvailable={false}
+                  data={housekeeping_store.hk_criteria.housekeepers
+                    .filter(hk => hk.id !== this.user.id)
+                    .map(m => ({
+                      value: m.id.toString(),
+                      text: m.name,
+                    }))
+                    .sort((a, b) => a.text.toLowerCase().localeCompare(b.text.toLowerCase()))}
+                ></ir-select>
               </div>
-              {this.user.assigned_units.length > 0 && (
-                <div class="modal-body text-left p-0 mb-2">
-                  <ir-select
-                    firstOption={locales.entries.Lcz_nobody}
-                    selectedValue={this.selectedId}
-                    onSelectChange={e => (this.selectedId = e.detail)}
-                    LabelAvailable={false}
-                    data={housekeeping_store.hk_criteria.housekeepers
-                      .filter(hk => hk.id !== this.user.id)
-                      .map(m => ({
-                        value: m.id.toString(),
-                        text: m.name,
-                      }))
-                      .sort((a, b) => a.text.toLowerCase().localeCompare(b.text.toLowerCase()))}
-                  ></ir-select>
-                </div>
-              )}
+            )}
+          </div>
 
-              <div class={`ir-alert-footer border-0 d-flex justify-content-end`}>
-                <ir-button btn_color={'secondary'} btn_block text={locales.entries.Lcz_Cancel} name={'cancel'}></ir-button>
-                <ir-button isLoading={this.loadingBtn === 'confirm'} btn_color={'primary'} btn_block text={locales.entries.Lcz_Confirm} name={'confirm'}></ir-button>
-              </div>
-            </div>
+          <div class={`ir-alert-footer border-0  d-flex justify-content-end`}>
+            <ir-button btn_color={'secondary'} btn_block text={locales.entries.Lcz_Cancel} name={'cancel'}></ir-button>
+            <ir-button isLoading={this.loadingBtn === 'confirm'} btn_color={'primary'} btn_block text={locales.entries.Lcz_Confirm} name={'confirm'}></ir-button>
           </div>
         </div>
-      </div>
-    );
+      </div>,
+    ];
+    // <div aria-modal={this.isOpen ? 'true' : 'false'} class="modal fade" ref={el => (this.modalEl = el)} tabindex="-1">
+    //   <div class="modal-dialog modal-dialog-centered">
+    //     <div class="modal-content">
+    //       <div class="modal-body">
+    //         <div class={`ir-alert-header mb-1 d-flex align-items-center justify-content-between border-0 py-0 m-0 `}>
+    //           <p class="p-0 my-0 modal-title">{this.user.assigned_units.length > 0 ? locales.entries.Lcz_AssignUnitsTo : locales.entries.Lcz_ConfirmDeletion}</p>
+    //           <ir-button class="exit-icon" variant="icon" icon_name="xmark" onClickHandler={() => this.closeModal()}></ir-button>
+    //         </div>
+    //         {this.user.assigned_units.length > 0 && (
+    //           <div class="modal-body text-left p-0 mb-2">
+    //             <ir-select
+    //               firstOption={locales.entries.Lcz_nobody}
+    //               selectedValue={this.selectedId}
+    //               onSelectChange={e => (this.selectedId = e.detail)}
+    //               LabelAvailable={false}
+    //               data={housekeeping_store.hk_criteria.housekeepers
+    //                 .filter(hk => hk.id !== this.user.id)
+    //                 .map(m => ({
+    //                   value: m.id.toString(),
+    //                   text: m.name,
+    //                 }))
+    //                 .sort((a, b) => a.text.toLowerCase().localeCompare(b.text.toLowerCase()))}
+    //             ></ir-select>
+    //           </div>
+    //         )}
+
+    //         <div class={`ir-alert-footer border-0 d-flex justify-content-end`}>
+    //           <ir-button btn_color={'secondary'} btn_block text={locales.entries.Lcz_Cancel} name={'cancel'}></ir-button>
+    //           <ir-button isLoading={this.loadingBtn === 'confirm'} btn_color={'primary'} btn_block text={locales.entries.Lcz_Confirm} name={'confirm'}></ir-button>
+    //         </div>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </div>
   }
 }
